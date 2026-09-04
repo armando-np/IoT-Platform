74f7dcdaaf3b358ea70bf3c96cd71528017cafcc
Armando Nunez <armando.nupa@gmail.com>
2026-09-04 12:02:57 -0600

    Integrate EMQX Cloud backend

--- apps/api/src/modules/mqtt/mqtt.module.ts
+++ apps/api/src/modules/mqtt/mqtt.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MqttConsumerService } from './mqtt-consumer.service';
 import { MqttPublisherService } from './mqtt-publisher.service';
+import { RealtimeModule } from '../realtime/realtime.module';
 
-@Module({ providers: [MqttConsumerService, MqttPublisherService], exports: [MqttPublisherService] })
+@Module({
+  imports: [RealtimeModule], providers: [MqttConsumerService, MqttPublisherService], exports: [MqttPublisherService] })
 export class MqttModule {}
